de0bd4ed63a2e40750126ddc0f77a650336f5fa5
Ruthwar Kumar Ambeer <ruthwar@codeaurora.org>
2015-11-30 16:18:58 +0530

    Update preferred network type based on active Sub ID
    On subInfo changed, Network type need not to be
    updated if SUB is not active or dummy.
    
    Keep the phone id based network type in sync with
    Sub ID based network type always.
    
    Change-Id: Idaa9da264321307f3f757f086b171f7e783a229a

--- src/java/com/android/internal/telephony/PhoneBase.java
+++ src/java/com/android/internal/telephony/PhoneBase.java
@@ -1124,7 +1124,7 @@ public abstract class PhoneBase extends Handler implements Phone {
 
     private void updateSavedNetworkOperator(NetworkSelectMessage nsm) {
         int subId = getSubId();
-        if (SubscriptionManager.isValidSubscriptionId(subId)) {
+        if (SubscriptionController.getInstance().isActiveSubId(subId)) {
             // open the shared preferences editor, and write the value.
             // nsm.operatorNumeric is "" if we're in automatic.selection.
             SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
@@ -2705,7 +2705,7 @@ public abstract class PhoneBase extends Handler implements Phone {
         // Update the cached value
         mRadioCapability.set(rc);
 
-        if (SubscriptionManager.isValidSubscriptionId(getSubId())) {
+        if (SubscriptionController.getInstance().isActiveSubId(getSubId())) {
             sendSubscriptionSettings(true);
         }
     }
@@ -2723,7 +2723,7 @@ public abstract class PhoneBase extends Handler implements Phone {
 
     protected void setPreferredNetworkTypeIfSimLoaded() {
         int subId = getSubId();
-        if (SubscriptionManager.isValidSubscriptionId(subId)) {
+        if (SubscriptionController.getInstance().isActiveSubId(subId)) {
             int type = PhoneFactory.calculatePreferredNetworkType(mContext, getSubId());
             setPreferredNetworkType(type, null);
         }
