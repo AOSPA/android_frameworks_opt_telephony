b10fff081e769e926ad3c47a72cad5fea6ed62f7
Suchand Ghosh <suchan@codeaurora.org>
2016-03-21 12:35:47 +0530

    IMS: Fix phone app crash at setOutgoingCallerIdDisplay().
    Phone process crash due to duplicate callback received at
    GsmPhone when launch additional call settings and click on
    Caller ID->Hide number.
    Correct logic to receive messge EVENT_SET_CLIR_COMPLETE by
    the requested phone (GsmPhone or ImsPhone), not by both.
    
    Change-Id: Iec19577b7bf567c769089c40c05c656754b694ae
    CRs-Fixed: 990931

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -1332,8 +1332,7 @@ public class GSMPhone extends PhoneBase {
         ImsPhone imsPhone = mImsPhone;
         if ((imsPhone != null)
                 && (imsPhone.getServiceState().getState() == ServiceState.STATE_IN_SERVICE)) {
-            imsPhone.setOutgoingCallerIdDisplay(commandInterfaceCLIRMode,
-            obtainMessage(EVENT_SET_CLIR_COMPLETE, commandInterfaceCLIRMode, 0, onComplete));
+            imsPhone.setOutgoingCallerIdDisplay(commandInterfaceCLIRMode, onComplete);
             return;
         }
         mCi.setCLIR(commandInterfaceCLIRMode,
