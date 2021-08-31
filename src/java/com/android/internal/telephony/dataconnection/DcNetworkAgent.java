f41ba484a84b08ced4e31f97f179ddb4d9f2fc45
Jack Yu <jackyu@google.com>
2021-08-31 00:07:45 -0700

    Merged concurent modified exception fix to keystone
    Bug: 201372093
    
    Fixed the concurrent modified exception
    
    The static map could be accessed by multiple threads. Changed
    to a thread-safe map.
    
    Fix: 197776892
    Test: Manual
    Merged-In: Ife1624c2f8fb02421ffeebfd6749d12bff7d8d68
    Change-Id: Ife1624c2f8fb02421ffeebfd6749d12bff7d8d68
    (cherry picked from commit b59e2ea4d65e32a76bc8c13d403e36f5b826d0b5)

--- src/java/com/android/internal/telephony/dataconnection/DcNetworkAgent.java
+++ src/java/com/android/internal/telephony/dataconnection/DcNetworkAgent.java
@@ -42,7 +42,6 @@ import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.telephony.data.NrQosSessionAttributes;
 import android.telephony.data.QosBearerSession;
-import android.util.ArrayMap;
 import android.util.LocalLog;
 import android.util.SparseArray;
 
@@ -64,6 +63,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -104,7 +104,7 @@ public class DcNetworkAgent extends NetworkAgent {
     private int mScore;
 
     // For interface duplicate detection. Key is the net id, value is the interface name in string.
-    private static Map<Integer, String> sInterfaceNames = new ArrayMap<>();
+    private static Map<Integer, String> sInterfaceNames = new ConcurrentHashMap<>();
 
     private final long NETWORK_LINGER_TIME_NON_DDS = 2000;      // In milliseconds
     private final int TEARDOWN_DELAY_TIMEOUT_NON_DDS = 3000;    // In milliseconds
