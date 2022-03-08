8c4978290b9c9dcf984d418235894d8fe8e852ca
Avinash Nalluri <quic_analluri@quicinc.com>
2022-03-08 10:35:21 -0800

    Port linger/delay timer changes to Android T
    Signed-off-by: Vala Zadeh <quic_vzadeh@quicinc.com>
    Change-Id: I58961625241a97659e672e083f75740bf94d5ac7
    CRs-Fixed: 3146156

--- src/java/com/android/internal/telephony/data/TelephonyNetworkAgent.java
+++ src/java/com/android/internal/telephony/data/TelephonyNetworkAgent.java
@@ -48,15 +48,15 @@ import java.util.concurrent.Executor;
  * has an associated parent {@link DataNetwork}.
  */
 public class TelephonyNetworkAgent extends NetworkAgent implements NotifyQosSessionInterface {
-    private final String mLogTag;
-    private final Phone mPhone;
+    protected String mLogTag;
+    protected final Phone mPhone;
     private final LocalLog mLocalLog = new LocalLog(128);
 
     /** The parent data network. */
-    private final @NonNull DataNetwork mDataNetwork;
+    protected final @NonNull DataNetwork mDataNetwork;
 
     /** This is the id from {@link NetworkAgent#register()}. */
-    private final int mId;
+    protected final int mId;
 
     /**
      * The callbacks that are used to pass information to {@link DataNetwork} and
@@ -315,7 +315,7 @@ public class TelephonyNetworkAgent extends NetworkAgent implements NotifyQosSess
      * Log error messages.
      * @param s error messages
      */
-    private void loge(@NonNull String s) {
+    protected void loge(@NonNull String s) {
         Rlog.e(mLogTag, s);
     }
 
