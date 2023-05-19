ef2c536c81d70d0c052141dcd5bb0c8118439ee2
Anand Jha <quic_anajha@quicinc.com>
2023-05-19 10:37:43 +0530

    Add null check in UiccSlot object to avoid NPE.
    When the SIM is removed, SIM CARD state moves to ABSENT state and
    UiccSlot will be disposed. Here UiccController is trying to read
    the portIndex from UiccSlot object which is already disposed.
    
    Change-Id: Id7ea5eaa0aa0176479384664179e17525077591c
    CRs-Fixed: 3493571

--- src/java/com/android/internal/telephony/uicc/UiccController.java
+++ src/java/com/android/internal/telephony/uicc/UiccController.java
@@ -850,7 +850,8 @@ public class UiccController extends Handler {
             }
             Rlog.d(LOG_TAG, "Broadcasting intent ACTION_SIM_CARD_STATE_CHANGED "
                     + TelephonyManager.simStateToString(state) + " for phone: " + phoneId
-                    + " slot: " + slotId + " port: " + slot.getPortIndexFromPhoneId(phoneId));
+                    + " slot: " + slotId + " port: "
+                    + (slot != null ? slot.getPortIndexFromPhoneId(phoneId) : null));
             mContext.sendBroadcast(intent, Manifest.permission.READ_PRIVILEGED_PHONE_STATE);
             TelephonyMetrics.getInstance().updateSimState(phoneId, state);
         }
@@ -889,7 +890,7 @@ public class UiccController extends Handler {
             Rlog.d(LOG_TAG, "Broadcasting intent ACTION_SIM_APPLICATION_STATE_CHANGED "
                     + TelephonyManager.simStateToString(state)
                     + " for phone: " + phoneId + " slot: " + slotId + "port: "
-                    + slot.getPortIndexFromPhoneId(phoneId));
+                    + (slot != null ? slot.getPortIndexFromPhoneId(phoneId) : null));
             mContext.sendBroadcast(intent, Manifest.permission.READ_PRIVILEGED_PHONE_STATE);
             TelephonyMetrics.getInstance().updateSimState(phoneId, state);
         }
