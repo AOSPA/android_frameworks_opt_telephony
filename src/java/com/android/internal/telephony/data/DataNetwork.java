b990e267f9cabd131b1e84dd751f28987db91066
Jake Weinstein <jake@aospa.co>
2023-06-17 17:55:15 +0900

    Revert "[REVERTME] Revert "Stick with perferred data profile""
    We have new blobs.
    
    This reverts commit 289f4ac7c077c8ffb516bbac547070f272e0e27b.
    
    Change-Id: I69226c22080e512236474e515ab05717cd5fd85d

--- src/java/com/android/internal/telephony/data/DataNetwork.java
+++ src/java/com/android/internal/telephony/data/DataNetwork.java
@@ -3183,7 +3183,7 @@ public class DataNetwork extends StateMachine {
                 && !mAttachedNetworkRequestList.isEmpty()) {
             TelephonyNetworkRequest networkRequest = mAttachedNetworkRequestList.get(0);
             DataProfile dataProfile = mDataNetworkController.getDataProfileManager()
-                    .getDataProfileForNetworkRequest(networkRequest, targetNetworkType, true);
+                    .getDataProfileForNetworkRequest(networkRequest, targetNetworkType, false);
             if (dataProfile != null) {
                 mHandoverDataProfile = dataProfile;
                 log("Used different data profile for handover. " + mDataProfile);
